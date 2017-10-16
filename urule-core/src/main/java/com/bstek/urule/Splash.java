dd9905098be0af25967ba577ea49aec04519ea60
jacky6024 <jacky6024@sina.com>
2017-10-16 15:22:11 +0800

    改新启动Splash，风格与UReport、UFLO保持一致

--- urule-core/src/main/java/com/bstek/urule/Splash.java
+++ urule-core/src/main/java/com/bstek/urule/Splash.java
@@ -8,19 +8,15 @@ public class Splash {
 	public void print(){
 		StringBuilder sb=new StringBuilder();
 		sb.append("\n");
-		sb.append("`7MMF'   `7MF'`7MM\"\"\"Mq.  `7MMF'   `7MF'`7MMF'      `7MM\"\"\"YMM           ");
+		sb.append("_____  __________ _____  ________ ________________ ");
 		sb.append("\n");
-		sb.append("  MM       M    MM   `MM.   MM       M    MM          MM    `7           ");
+		sb.append("__  / / /___  __ \\__  / / /___  / ___  ____/__|__ \\");
 		sb.append("\n");
-		sb.append("  MM       M    MM   ,M9    MM       M    MM          MM   d     pd*\"*b. ");
+		sb.append("_  / / / __  /_/ /_  / / / __  /  __  __/   ____/ /");
 		sb.append("\n");
-		sb.append("  MM       M    MMmmdM9     MM       M    MM          MMmmMM    (O)   j8 ");
+		sb.append("/ /_/ /  _  _, _/ / /_/ /  _  /____  /___   _  __/ ");
 		sb.append("\n");
-		sb.append("  MM       M    MM  YM.     MM       M    MM      ,   MM   Y  ,     ,;j9 ");
-		sb.append("\n");
-		sb.append("  YM.     ,M    MM   `Mb.   YM.     ,M    MM     ,M   MM     ,M  ,-='    ");
-		sb.append("\n");
-		sb.append("   `bmmmmd\"'  .JMML. .JMM.   `bmmmmd\"'  .JMMmmmmMMM .JMMmmmmMMM Ammmmmmm ");
+		sb.append("\\____/   /_/ |_|  \\____/   /_____//_____/   /____/ ");
 		sb.append("\n");
 		sb.append(".....................................................................................................");
 		sb.append("\n");
