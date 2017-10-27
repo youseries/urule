e52f55e06ffe3a568421c650752cadad4369cf0a
jacky6024 <jacky6024@sina.com>
2017-10-27 09:22:50 +0800

    LICENSE变更

--- urule-core/src/main/java/com/bstek/urule/Splash.java
+++ urule-core/src/main/java/com/bstek/urule/Splash.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Bstek.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package com.bstek.urule;
 
 /**
@@ -21,10 +37,10 @@ public class Splash {
 		sb.append(".....................................................................................................");
 		sb.append("\n");
 		sb.append(".  uRule, is a Chinese style rule engine");
-		sb.append(" licensed under the Apache License 2.0,                     .");
+		sb.append(" licensed under GNU GENERAL PUBLIC LICENSE Version 3,       .");
 		sb.append("\n");
-		sb.append(".  which is opensource, free of charge, easy to use,");
-		sb.append("high-performance, with browser-based-designer.  .");
+		sb.append(".  which is opensource, easy to use,");
+		sb.append("high-performance, with browser-based-designer.                  .");
 		sb.append("\n");
 		sb.append(".....................................................................................................");
 		sb.append("\n");
