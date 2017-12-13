857c30adb697389dfbdcf1918b3ae2aff190876b
jacky6024 <jacky6024@sina.com>
2017-12-13 15:22:37 +0800

    优化URuleServlet中Spring上下文对象获取方式，以方便其它项目扩展

--- urule-console/src/main/java/com/bstek/urule/console/servlet/URuleServlet.java
+++ urule-console/src/main/java/com/bstek/urule/console/servlet/URuleServlet.java
@@ -46,7 +46,7 @@ public class URuleServlet extends HttpServlet{
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		WebApplicationContext applicationContext=WebApplicationContextUtils.getWebApplicationContext(config.getServletContext());
+		WebApplicationContext applicationContext=getWebApplicationContext(config);
 		Collection<ServletHandler> handlers=applicationContext.getBeansOfType(ServletHandler.class).values();
 		for(ServletHandler handler:handlers){
 			String url=handler.url();
@@ -55,6 +55,10 @@ public class URuleServlet extends HttpServlet{
 			}
 			handlerMap.put(url, handler);
 		}
+	}
+	
+	protected WebApplicationContext getWebApplicationContext(ServletConfig config){
+		return WebApplicationContextUtils.getWebApplicationContext(config.getServletContext());
 	}
 	
 	@Override
