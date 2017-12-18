ffbc23c99e0066383902ce983fba634a35c2f344
jacky6024 <jacky6024@sina.com>
2017-12-18 15:17:08 +0800

    添加RepositoryReader接口给开发人员使用，以便于读取知识库信息

--- urule-console/src/main/java/com/bstek/urule/console/repository/RepositoryReader.java
+++ urule-console/src/main/java/com/bstek/urule/console/repository/RepositoryReader.java
@@ -27,25 +27,26 @@ import com.bstek.urule.console.repository.model.VersionFile;
  * @since 2017年12月15日
  */
 public interface RepositoryReader {
+	public static final String BEAN_ID=RepositoryService.BEAN_ID;
 	/**
 	 * 加载指定的companyId下创建的所有的项目信息
 	 * @param companyId 指定的公司ID
 	 * @return 返回所有的项目信息
-	 * @throws Exception
+	 * @throws Exception 抛出异常
 	 */
 	List<RepositoryFile> loadProjects(String companyId) throws Exception;
 	/**
 	 * 读取指定的最新版本的文件
 	 * @param path 文件考路径
 	 * @return 返回文件内容
-	 * @throws Exception
+	 * @throws Exception 抛出异常
 	 */
 	InputStream readFile(String path) throws Exception;
 	/**
 	 * 加载指定项目下的知识包信息
 	 * @param project 项目名称
 	 * @return 返回已定义的知识包信息
-	 * @throws Exception
+	 * @throws Exception 抛出异常
 	 */
 	List<ResourcePackage> loadProjectResourcePackages(String project) throws Exception;
 	/**
@@ -61,7 +62,7 @@ public interface RepositoryReader {
 	 * @param path 文件路径
 	 * @param version 文件版本号
 	 * @return 返回文件内容
-	 * @throws Exception
+	 * @throws Exception 抛出异常
 	 */
 	InputStream readFile(String path, String version) throws Exception;
 }
